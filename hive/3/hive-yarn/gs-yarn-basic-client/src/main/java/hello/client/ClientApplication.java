79015e65e41c5fa955ddcc6273772e61f8c65b38
vera_sidarovich <vera_sidarovich@epam.com>
2015-11-05 13:57:16 +0300

    done 3rd hw

--- hive/3/hive-yarn/gs-yarn-basic-client/src/main/java/hello/client/ClientApplication.java
+++ hive/3/hive-yarn/gs-yarn-basic-client/src/main/java/hello/client/ClientApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.yarn.client.YarnClient;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class ClientApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(ClientApplication.class, args);
+         SpringApplication.run(ClientApplication.class, args);
 	}
-
 }
--- hive/3/hive-yarn/gs-yarn-basic-container/src/main/java/hello/container/HelloPojo.java
+++ hive/3/hive-yarn/gs-yarn-basic-container/src/main/java/hello/container/HelloPojo.java
@@ -4,10 +4,17 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.hadoop.fs.FsShell;
 import org.springframework.yarn.annotation.OnContainerStart;
 import org.springframework.yarn.annotation.YarnComponent;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 
 @YarnComponent
 public class HelloPojo {
@@ -19,13 +26,15 @@ public class HelloPojo {
 
 	@OnContainerStart
 	public void publicVoidNoArgsMethod() throws Exception {
-		log.info("Hello from HelloPojo");
-		log.info("About to list from hdfs root content");
-
-		FsShell shell = new FsShell(configuration);
-		for (FileStatus s : shell.ls(false, "/")) {
-			log.info(s);
-		}
-		shell.close();
+		log.info("Request result:");
+		FileSystem fs = FileSystem.get(URI.create("hdfs://10.0.2.15"), new Configuration());
+        String uri = "/user/root/planes.output/stdout";
+		BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(new Path(uri))));
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            log.info(line);
+        }
+        log.info("Done.");
 	}
+
 }
